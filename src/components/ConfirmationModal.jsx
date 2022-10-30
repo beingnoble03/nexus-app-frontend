e1ad13c5817217f03238301619695e3ab23163ef
Noble Mittal <noblemittal@outlook.com>
2022-10-30 16:55:55 +0530

    add toast to confirmation modal

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -1,70 +1,80 @@
 import { Button, Modal, Box, Typography } from "@mui/material";
 import axios from "axios";
-
+import { toast } from "react-toastify";
 
 const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: { xs: 350, sm: 500 },
-    bgcolor: "background.paper",
-    border: "1px solid #000",
-    boxShadow: 24,
-    p: 4,
-    borderRadius: `10px`,
-    maxHeight: `90%`,
-    overflow: `scroll`,
-}
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: { xs: 350, sm: 500 },
+  bgcolor: "background.paper",
+  border: "1px solid #000",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: `10px`,
+  maxHeight: `90%`,
+  overflow: `scroll`,
+};
 
 const buttonContainerStyle = {
-    display: `flex`,
-    flexDirection: `row`,
-    justifyContent: `flex-end`,
-    gap: `20px`,
-    marginTop: `40px`,
-}
+  display: `flex`,
+  flexDirection: `row`,
+  justifyContent: `flex-end`,
+  gap: `20px`,
+  marginTop: `40px`,
+};
 
 export default function ConfirmationModal(props) {
   const open = props.openConfirmationModal;
   const handleAbortButton = () => {
-      props.setOpenConfirmationModal(false)
-  }
+    props.setOpenConfirmationModal(false);
+  };
   const handleConfirmButton = () => {
-      if (props.roundType === "I"){
+    if (props.roundType === "I") {
+      axios({
+        method: "post",
+        url: `http://localhost:8000/api/interviews/`,
+        headers: {
+          Authorization: "Token " + localStorage.getItem("token"),
+        },
+        data: {
+          applicant_ids: props.applicantIds,
+          round_id: props.roundId,
+          completed: false,
+        },
+      }).then((response) => {
+        console.log(response.data);
+      });
+    } else {
+      props.applicantIds.map((value, index) => {
         axios({
-            method: "post",
-            url: `http://localhost:8000/api/interviews/`,
-            headers: {
-              Authorization: "Token " + localStorage.getItem("token"),
-            },
-            data: {
-                applicant_ids: props.applicantIds,
-                round_id: props.roundId,
-                completed: false,
-            }
-          }).then((response) => {
-              console.log(response.data)
+          method: "patch",
+          url: `http://localhost:8000/api/applicants/${value}/`,
+          headers: {
+            Authorization: "Token " + localStorage.getItem("token"),
+          },
+          data: {
+            action: "add",
+            round: [props.roundId],
+          },
+        }).then((response) => {
+            props.setOpenConfirmationModal(false)
+          toast.success("Added selected applicants.", {
+            position: "bottom-right",
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            toastId: "success1",
           });
-      } else {
-          props.applicantIds.map((value, index) => {
-              axios({
-
-            method: "patch",
-            url: `http://localhost:8000/api/applicants/${value}/`,
-            headers: {
-              Authorization: "Token " + localStorage.getItem("token"),
-            },
-            data: {
-                action: "add",
-                round: [props.roundId, ],
-            }
-          }).then((response) => {
-              console.log(response.data)
-          });
-              })
-      }
-  }
+        });
+      });
+    }
+  };
 
   return (
     <Modal
@@ -81,8 +91,20 @@ export default function ConfirmationModal(props) {
           Moving the selected applicants to {String(props.roundName)}
         </Typography>
         <Box sx={buttonContainerStyle}>
-        <Button color="primary" variant="outlined" onClick={handleConfirmButton}>Confirm</Button>
-        <Button color="primary" variant="contained" onClick={handleAbortButton}>Abort</Button>
+          <Button
+            color="primary"
+            variant="outlined"
+            onClick={handleConfirmButton}
+          >
+            Confirm
+          </Button>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={handleAbortButton}
+          >
+            Abort
+          </Button>
         </Box>
       </Box>
     </Modal>
