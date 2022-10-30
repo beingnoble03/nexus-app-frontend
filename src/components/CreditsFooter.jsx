8678d74e40cb949bb31384afeb059fec35b5eaff
Noble Mittal <noblemittal@outlook.com>
2022-10-30 16:50:11 +0530

    init credits footer component

---
+++ src/components/CreditsFooter.jsx
@@ -0,0 +1,28 @@
+import { Box, Typography } from "@mui/material"
+
+
+const style = {
+    width: `100%`,
+    background: `radial-gradient(circle, rgba(218,111,158,1) 0%, rgba(25,118,210,1) 80%)`,
+    // bgcolor: `#1976d2`,
+    padding: `5px`,
+    color: `white`,
+    textAlign: `center`,
+    fontSize: `20px`,
+    position: `sticky`,
+    bottom: `0px`,
+    height: `auto`,
+}
+
+export default function CreditsFooter() {
+  return (
+    <Box sx={style}>
+        <Typography variant="subtitle1">
+            {"Made with 🤍 by "} <a href="https://github.com/beingnoble03" style={{
+                textDecoration: `none`,
+                color: `white`,
+            }}><b>Noble Mittal</b></a>
+        </Typography>
+    </Box>
+  )
+}
